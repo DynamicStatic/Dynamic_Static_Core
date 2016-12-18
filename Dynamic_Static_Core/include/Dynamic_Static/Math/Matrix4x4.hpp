3fad658dd9332c84b263a7a6f90690f525ae1789
Patrick Purcell <p.84@live.com>
2016-12-18 14:39:19 -0800

    Adding glm.

---
+++ Dynamic_Static_Core/include/Dynamic_Static/Math/Matrix4x4.hpp
@@ -0,0 +1,78 @@
+
+/*
+=====================================================================================
+
+    The MIT License(MIT)
+
+    Copyright(c) 2016 to this->moment()->next() Dynamic_Static
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+=====================================================================================
+*/
+
+#pragma once
+
+#include "Dynamic_Static/Math/Defines.hpp"
+
+#pragma warning(push, 0)
+#include "glm-master/glm/glm.hpp"
+#pragma warning(pop)
+
+namespace Dynamic_Static {
+    namespace Math {
+        /**
+         * TODO : Documentation.
+         */
+        struct Matrix4x4 final
+            : public glm::mat4x4 {
+            using glm::mat4x4::tmat4x4;
+
+            /**
+             * Constructs an instance of Matrix4x4.
+             */
+            Matrix4x4() = default;
+
+            /**
+             * TODO : Documentation.
+             */
+            Matrix4x4(const glm::mat4x4& other);
+
+            /**
+             * TODO : Documentation.
+             */
+            Matrix4x4& operator=(const glm::mat4x4& other);
+
+            /**
+             * TODO : Documentation.
+             */
+            void rotate(float angle, const Vector3& axis);
+
+            /**
+             * TODO : Documentation.
+             */
+            void look_at(const Vector3& eye, const Vector3& center, const Vector3& up);
+
+            /**
+             * TODO : Documentation.
+             */
+            static Matrix4x4 perspective(float fov_y, float aspect_ratio, float near_plane, float far_plane);
+        };
+    }
+}
---
+++ Dynamic_Static_Core/src/Dynamic_Static/Math/Matrix4x4.cpp
@@ -0,0 +1,66 @@
+
+/*
+=====================================================================================
+
+    The MIT License(MIT)
+
+    Copyright(c) 2016 to this->moment()->next() Dynamic_Static
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+=====================================================================================
+*/
+
+#include "Dynamic_Static/Math/Matrix4x4.hpp"
+
+#include "Dynamic_Static/Math/Vector3.hpp"
+
+#pragma warning(push, 0)
+#include "glm-master/glm/gtc/matrix_transform.hpp"
+#pragma warning(pop)
+
+namespace Dynamic_Static {
+    namespace Math {
+        Matrix4x4::Matrix4x4(const glm::mat4x4& other)
+            : glm::mat4x4(other)
+        {
+        }
+
+        Matrix4x4& Matrix4x4::operator=(const glm::mat4x4& other)
+        {
+            glm::mat4x4::operator=(other);
+            return *this;
+        }
+
+        void Matrix4x4::rotate(float angle, const Vector3& axis)
+        {
+            glm::rotate(*this, angle, axis);
+        }
+
+        void Matrix4x4::look_at(const Vector3& eye, const Vector3& center, const Vector3& up)
+        {
+            *this = glm::lookAt(eye, center, up);
+        }
+
+        Matrix4x4 Matrix4x4::perspective(float fov_y, float aspect_ratio, float near_plane, float far_plane)
+        {
+            return glm::perspective(fov_y, aspect_ratio, near_plane, far_plane);
+        }
+    }
+}
