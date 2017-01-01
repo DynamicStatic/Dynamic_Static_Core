379ee01a020f86e2e7fc73b9e9be003c78d7b254
Patrick Purcell <p.84@live.com>
2016-12-31 16:59:52 -0800

    Swapping Vector3::left and Vector3::right.

--- Dynamic_Static_Core/src/Dynamic_Static/Math/Vector3.cpp
+++ Dynamic_Static_Core/src/Dynamic_Static/Math/Vector3.cpp
@@ -76,8 +76,8 @@ namespace Dynamic_Static {
         const Vector3 Vector3::zero     = Vector3 {  0,  0,  0 };
         const Vector3 Vector3::up       = Vector3 {  0,  1,  0 };
         const Vector3 Vector3::down     = Vector3 {  0, -1,  0 };
-        const Vector3 Vector3::left     = Vector3 { -1,  0,  0 };
-        const Vector3 Vector3::right    = Vector3 {  1,  0,  0 };
+        const Vector3 Vector3::left     = Vector3 {  1,  0,  0 };
+        const Vector3 Vector3::right    = Vector3 { -1,  0,  0 };
         const Vector3 Vector3::forward  = Vector3 {  0,  0,  1 };
         const Vector3 Vector3::backward = Vector3 {  0,  0, -1 };
         const Vector3 Vector3::unit_x   = Vector3 {  1,  0,  0 };
