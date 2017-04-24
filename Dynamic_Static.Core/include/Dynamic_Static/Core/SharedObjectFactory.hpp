3b60f7a77ea9c4cffc5a76866db60254869434d2
Patrick Purcell <p.84@live.com>
2017-04-23 22:10:17 -0700

    Adding SharedObjectFactory

---
+++ Dynamic_Static.Core/include/Dynamic_Static/Core/SharedObjectFactory.hpp
@@ -0,0 +1,113 @@
+
+/*
+=====================================================================================
+
+    The MIT License(MIT)
+
+    Copyright(c) 2017 to this->moment()->next() Dynamic_Static
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+
+=====================================================================================
+*/
+
+#pragma once
+
+#include "Dynamic_Static/Core/Defines.hpp"
+#include "Dynamic_Static/Core/Collection.hpp"
+#include "Dynamic_Static/Core/NonCopyable.hpp"
+#include "Dynamic_Static/Core/TypeUtilities.hpp"
+
+#include <tuple>
+#include <array>
+#include <vector>
+#include <memory>
+#include <cstddef>
+#include <utility>
+#include <type_traits>
+
+namespace Dynamic_Static {
+    template <typename ...ObjectTypes>
+    class SharedObjectFactory {
+
+    private:
+        mutable std::tuple<std::vector<std::weak_ptr<ObjectTypes>>...> mSharedObjects;
+
+    public:
+        /**
+         * Constructs an instance of SharedObjectFactory.
+         */
+        SharedObjectFactory() = default;
+
+        /**
+         * Moves an instance of SharedObjectFactory.
+         * @param [in] other The SharedObjectFactory to move from
+         */
+        SharedObjectFactory(SharedObjectFactory&& other) = default;
+
+        /**
+         * Moves an instance of SharedObjectFactory.
+         * @param [in] other The SharedObjectFactory to move from
+         */
+        SharedObjectFactory& operator=(SharedObjectFactory&& other) = default;
+
+    public:
+        template <typename ObjectType>
+        const Collection<std::weak_ptr<ObjectType>> objects() const
+        {
+            return validated_objects<ObjectType>();
+        }
+
+        /**
+         * Creates a shared object.
+         * @param <ObjectType> The type of shared object to create
+         * @param <Args> Arguments to the shared object's constructor
+         * @return The newly created shared object
+         */
+        template <typename ObjectType, typename ...Args>
+        std::shared_ptr<ObjectType> create(Args&&... args)
+        {
+            // TODO : Accept an allocator and use std::allocate_shared<>().
+            auto sharedObject = std::make_shared<ObjectType>(std::forward<Args>(args)...);
+            validated_objects<ObjectType>().push_back(sharedObject);
+            return sharedObject;
+        }
+
+    private:
+        template <typename ObjectType>
+        std::vector<std::weak_ptr<ObjectType>>& validated_objects() const
+        {
+            constexpr size_t index = dst::type_index<ObjectType, ObjectTypes...>();
+            auto& objects = std::get<index>(mSharedObjects);
+            objects.erase(
+                std::remove_if(
+                    objects.begin(),
+                    objects.end(),
+                    [](std::weak_ptr<ObjectType> object)
+                    {
+                        return object.expired();
+                    }
+                ),
+                objects.end()
+            );
+
+            return objects;
+        }
+    };
+} // namespace Dynamic_Static
--- Sandbox/Main.cpp
+++ Sandbox/Main.cpp
@@ -30,12 +30,14 @@
 #include "Dynamic_Static/Core/Dependency.hpp"
 #include "Dynamic_Static/Core/Collection.hpp"
 #include "Dynamic_Static/Core/NonCopyable.hpp"
+#include "Dynamic_Static/Core/SharedObjectFactory.hpp"
 
 #include <string>
 #include <iostream>
 
 #include <vector>
 #include <memory>
+#include <utility>
 #include <functional>
 
 #if 0
@@ -174,50 +176,53 @@ namespace Dynamic_Static {
 } // namespace Dynamic_Static
 #endif
 
-class Widget final {
+// class Widget final {
+// 
+// };
+// 
+// class Woozle final {
+// 
+// };
+// 
+// template <typename T>
+// class Factory {
+// private:
+//     std::vector<T> mObjects;
+// 
+// public:
+//     void create_object()
+//     {
+//         mObjects.push_back(T());
+//     }
+// 
+//     const std::vector<T>& objects() const
+//     {
+//         return mObjects;
+//     }
+// };
+// 
+// class Machine final
+//     : public Factory<Widget>
+//     , public Factory<Woozle> {
+// public:
+//     template <typename T>
+//     void create_object()
+//     {
+//         Factory<T>::create_object();
+//     }
+// 
+//     template <typename T>
+//     const std::vector<T>& objects() const
+//     {
+//         return Factory<T>::objects();
+//     }
+// };
 
-};
-
-class Woozle final {
-
-};
-
-template <typename T>
-class Factory {
-private:
-    std::vector<T> mObjects;
-
-public:
-    void create_object()
-    {
-        mObjects.push_back(T());
-    }
-
-    const std::vector<T>& objects() const
-    {
-        return mObjects;
-    }
-};
-
-class Machine final
-    : public Factory<Widget>
-    , public Factory<Woozle> {
-public:
-    template <typename T>
-    void create_object()
-    {
-        Factory<T>::create_object();
-    }
-
-    template <typename T>
-    const std::vector<T>& objects() const
-    {
-        return Factory<T>::objects();
-    }
-};
+int main_ex();
 
 int main(/* int argc, char* argv[] */)
 {
+    return main_ex();
     // using namespace dst;
     // auto physicalDevice = std::make_shared<PhysicalDevice>();
     // auto sp = physicalDevice->shared_from_this();
@@ -235,11 +240,117 @@ int main(/* int argc, char* argv[] */)
     // auto devices1 = physicalDevice->devices();
     // int break1 = 1;
 
-    Machine machine;
-    machine.create_object<Widget>();
-    machine.create_object<Woozle>();
-    auto widgets = machine.objects<Widget>();
-    auto woozles = machine.objects<Woozle>();
+    // Machine machine;
+    // machine.create_object<Widget>();
+    // machine.create_object<Woozle>();
+    // auto widgets = machine.objects<Widget>();
+    // auto woozles = machine.objects<Woozle>();
+
+    return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace Dynamic_Static {
+    class Widget final {
+
+    private:
+        float mValue { };
+
+    public:
+        Widget(float value)
+            : mValue { value }
+        {
+        }
+
+    public:
+        float value() const
+        {
+            return mValue;
+        }
+
+        // template <typename ...Args>
+        // static std::shared_ptr<Widget> create(Args&&... args)
+        // {
+        //     return std::shared_ptr<Widget>(new Widget(std::forward<Args>(args)...));
+        // }
+    };
+} // namespace Dynamic_Static
+
+namespace Dynamic_Static {
+    class Whatsit final {
+
+    private:
+        size_t mValue { };
+
+    public:
+        Whatsit(size_t value)
+            : mValue { value }
+        {
+        }
+
+    public:
+        size_t value() const
+        {
+            return mValue;
+        }
+    };
+} // namespace Dynamic_Static
+
+namespace Dynamic_Static {
+    class Machine final
+        : SharedObjectFactory<Widget>
+        , SharedObjectFactory<Whatsit>
+        //, public ISharedObjectFactory
+    {
+    public:
+        template <typename T, typename ...Args>
+        std::shared_ptr<T> create(Args&&... args)
+        {
+            return SharedObjectFactory<T>::create(std::forward<Args>(args)...);
+        }
+
+    private:
+        // template <typename T>
+        // Widget* allocate() override
+        // {
+        //     return new Widget(3.14f);
+        // }
+    };
+}  // namespace Dynamic_Static
+
+int main_ex()
+{
+    using namespace Dynamic_Static;
+    //Machine machine;
+    //machine.create<Widget>(3.14f);
+
+    std::tuple<int, float> t;
+
+    //std::get<0>(t) = 4;
+    //std::get<1>(t) = 3.14f;
+
+    std::get<float>(t) = 3.14f;
+
+    SharedObjectFactory<
+        Widget,
+        Whatsit
+    > factory;
+    auto w0 = factory.create<Widget>(3.14f);
+    auto w1 = factory.create<Whatsit>(16);
+    auto w2 = factory.create<Widget>(64.0f);
+    auto ws = factory.objects<Widget>();
+
+    //List<TypeList<A, B, C>>::type myTuple;
+    //auto& a = std::get<A>(myTuple);
+    //auto& b = std::get<B>(myTuple);
+    //auto& c = std::get<C>(myTuple);
+
+    //List<TypeList<A, B, C>> myList;
+    //myList.push_back<A>(A { });
+    //myList.push_back<C>(C { });
+    //myList.push_back<C>(C { });
 
     return 0;
 }
