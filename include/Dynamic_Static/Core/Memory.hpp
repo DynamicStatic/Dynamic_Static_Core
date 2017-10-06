11717756b8df6433e17977e5bea97d977c2581f1
Patrick Purcell <p.84@live.com>
2017-10-05 22:03:26 -0700

    Adding memory utilities.

---
+++ include/Dynamic_Static/Core/Memory.hpp
@@ -0,0 +1,28 @@
+
+/*
+==========================================
+    Copyright (c) 2016 Dynamic_Static 
+    Licensed under the MIT license
+    http://opensource.org/licenses/MIT
+==========================================
+*/
+
+#pragma once
+
+#include "Dynamic_Static/Core/Defines.hpp"
+
+#include <memory>
+
+namespace Dynamic_Static {
+
+    /**
+     * TODO : Documentation.
+     */
+    void* aligned_alloc(size_t size, size_t alignment);
+
+    /**
+     * TODO : Documentation.
+     */
+    void aligned_free(void* data);
+
+} // namespace Dynamic_Static
