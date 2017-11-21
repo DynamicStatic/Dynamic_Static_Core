4b99203a4ca356d75a0d07140fa136a5e03205f0
patrick <ppurcell@immersivemedia.com>
2017-11-20 18:10:33 -0800

    Setting up BinaryReader tests

--- tests/source/BinaryReader.Tests.cpp
+++ tests/source/BinaryReader.Tests.cpp
@@ -1,25 +1,26 @@
 
 /*
 ==========================================
-    Copyright (c) 2016 Dynamic_Static
+    Copyright (c) 2017 Dynamic_Static
     Licensed under the MIT license
     http://opensource.org/licenses/MIT
 ==========================================
 */
 
-#include "Dynamic_Static/Core/Algorithm.hpp"
-#include "Dynamic_Static/Core/Random.hpp"
+#include "Dynamic_Static/Core/BinaryReader.hpp"
 
 #include "catch.hpp"
-
-#include <vector>
 
 namespace Dynamic_Static {
 namespace Tests {
 
-    TEST_CASE("wrap()", "[Algorithm]")
+    TEST_CASE("----", "[BinaryReader]")
     {
-        // REQUIRE(dst::wrap(0, 0, 0) != 0);
+        const char* loremIpsum =
+            #include "LoremIpsum.txt"
+            ;
+
+        REQUIRE(true);
     }
 
 } // namespace Tests
