27dd6c88d0773f6acb81313de83bd2036d03311b
PatrickPurcell <patrick.r.purcell@gmail.com>
2018-07-29 16:05:47 -0700

    Adding Dynamic_Static.Core.hpp

---
+++ include/Dynamic_Static.Core.hpp
@@ -0,0 +1,30 @@
+
+/*
+==========================================
+  Copyright (c) 2018-2018 Dynamic_Static
+    Patrick Purcell
+      Licensed under the MIT license
+    http://opensource.org/licenses/MIT
+==========================================
+*/
+
+#pragma once
+
+#include "Dynamic_Static/Core/Defines.hpp"
+#include "Dynamic_Static/Core/Action.hpp"
+#include "Dynamic_Static/Core/Algorithm.hpp"
+#include "Dynamic_Static/Core/Callback.hpp"
+#include "Dynamic_Static/Core/Events.hpp"
+#include "Dynamic_Static/Core/File.hpp"
+#include "Dynamic_Static/Core/FileSystem.hpp"
+#include "Dynamic_Static/Core/Math.hpp"
+#include "Dynamic_Static/Core/Memory.hpp"
+#include "Dynamic_Static/Core/MoveNotifier.hpp"
+#include "Dynamic_Static/Core/NonCopyable.hpp"
+#include "Dynamic_Static/Core/Random.hpp"
+#include "Dynamic_Static/Core/StringUtilities.hpp"
+#include "Dynamic_Static/Core/Threads.hpp"
+#include "Dynamic_Static/Core/Time.hpp"
+#include "Dynamic_Static/Core/VectorUtilities.hpp"
+#include "Dynamic_Static/Core/Version.hpp"
+#include "Dynamic_Static/Core/Win32LeanAndMean.hpp"
