73d7527c70cdc65c26fd944ae50fb1a3488f605b
PatrickPurcell <patrick.r.purcell@gmail.com>
2018-08-04 16:34:34 -0700

    General cleanup

--- external/gsl.cmake
+++ external/gsl.cmake
@@ -1,2 +1,9 @@
 
+#==========================================
+#  Copyright (c) 2017-2018 Dynamic_Static
+#    Patrick Purcell
+#      Licensed under the MIT license
+#    http://opensource.org/licenses/MIT
+#==========================================
+
 set(GSL.includeDirectories "${CMAKE_CURRENT_LIST_DIR}/GSL/include/")
