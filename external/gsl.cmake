c12a9e0c1fea471095f118e2e91bf34a9a4fe9e0
PatrickPurcell <patrick.r.purcell@gmail.com>
2018-08-04 17:58:33 -0700

    Removing gsl and tclap

--- external/gsl.cmake
+++
@@ -1,9 +0,0 @@
-
-#==========================================
-#  Copyright (c) 2017-2018 Dynamic_Static
-#    Patrick Purcell
-#      Licensed under the MIT license
-#    http://opensource.org/licenses/MIT
-#==========================================
-
-set(GSL.includeDirectories "${CMAKE_CURRENT_LIST_DIR}/GSL/include/")
--- external/tclap.cmake
+++
@@ -1,5 +0,0 @@
-
-set(tclap.includeDirectories "${CMAKE_CURRENT_LIST_DIR}/tclap/include/")
-list(APPEND tclap.clutter "${tclap.includeDirectories}/MakeFile.am")
-list(APPEND tclap.clutter "${tclap.includeDirectories}/tclap/MakeFile.am")
-file(REMOVE_RECURSE ${tclap.clutter})
